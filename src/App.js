022ea11808c40ae9ebdfb339a9736ba85cb065af
Rumer Tovar <rumertovar7474@gmail.com>
2022-01-02 14:57:45 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import PatternTop from './images/bg-pattern-top.svg';
+import PatternBotton from './images/bg-pattern-bottom.svg';
+import ProfilePicture from './images/image-victor.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+  <>
+   <div className='fatherContainer'>
+    <img className='PatternTop' src={PatternTop} alt='' />
+    <img className='PatternBottom' src={PatternBotton} alt='' />
+    <div className='container'>
+     <div className='card'>
+      <div className='header'>
+       <img
+        className='profile-picture'
+        src={ProfilePicture}
+        alt='victor crest'
+       />
+      </div>
+      <div className='content'>
+       <div className='content-top'>
+        <h2 className='name'>
+         Victor Crest <span>26</span>
+        </h2>
+        <p className='city'>London</p>
+       </div>
+       <div className='content-bottom'>
+        <div className='Followers'>
+         <h2>80k</h2>
+         <h4>Followers</h4>
+        </div>
+        <div className='Likes'>
+         <h2>803K</h2>
+         <h4>Likes</h4>
+        </div>
+        <div className='Photos'>
+         <h2>1.4K</h2>
+         <h4>Photos</h4>
+        </div>
+       </div>
+      </div>
+     </div>
     </div>
-  );
+   </div>
+  </>
+ );
 }
 
 export default App;
